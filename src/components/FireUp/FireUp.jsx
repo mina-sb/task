5eda33379b519bd06e0af84525387df23fd9f130
mina <sadeghbayan.mina@gmail.com>
2023-09-10 18:33:17 +0330

    FireUp made responsive

--- src/components/FireUp/FireUp.jsx
+++ src/components/FireUp/FireUp.jsx
@@ -4,12 +4,14 @@ import Button from '../UIElements/Button';
 
 const FireUp = () => {
   return (
-    <section className="bg-sky-50	mt-32 mb-10 pt-12 h-[17rem] rounded-2xl text-center mx-8 md:mx-16 xl:mx-24">
-      <div className="flex">
-        <img src={rocket} className="-mt-40 w-96 ml-28" />
-        <div className="ml-16 text-left">
+    <section className="bg-sky-50	mt-32 mb-10 pt-12 md:h-[14rem] lg:h-[17rem] rounded-2xl text-center mx-8 md:mx-16 xl:mx-24">
+      <div className="flex flex-col-reverse md:flex-row">
+        <div>
+          <img src={rocket} className="md:-mt-28 lg:-mt-40 hidden md:block md:w-72 lg:w-96 md:ml-10 lg:ml-20" />
+        </div>
+        <div className="ml-16 text-left pr-2">
           <h6
-            className={`text-[2.6rem] font-extrabold mt-6 text-slate-800 leading-normal inline-block`}
+            className={`text-2xl lg:text-[2.6rem] font-extrabold md:mt-1 lg:mt-6 text-slate-800 leading-normal inline-block`}
           >
             Fire up Your
             <span className="text-[#356dff]"> Rocket </span>!
@@ -20,7 +22,7 @@ const FireUp = () => {
           </p>
           <Button
             text="Let's discuss"
-            style="bg-slate-900 border-slate-900 border-2 text-white mt-8"
+            style="bg-slate-900 border-slate-900 border-2 text-white mt-8 mb-4"
           />
         </div>
       </div>{" "}
