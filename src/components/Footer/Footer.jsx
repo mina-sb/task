3191dff14b51083a3a928e8c02f7e04d1acc824a
mina <sadeghbayan.mina@gmail.com>
2023-09-10 20:36:26 +0330

    minor fixes in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,18 +12,18 @@ const Footer = () => {
           <span className="text-2xl lg:text-4xl mt-8 inline-block">
             Hello@Startup.com
           </span>
-          <i class="bx bx-right-arrow-alt text-base lg:text-xl rounded-full px-3 py-2 -rotate-45 text-white bg-[#356dff] ml-12"></i>
+          <i class="bx bx-right-arrow-alt text-base lg:text-xl rounded-full px-3 py-2 -rotate-45 text-white bg-[#356dff] hover:bg-slate-900 ml-12 duration-300 hover:cursor-pointer"></i>
           <div className="flex mt-8 items-center">
             <p className="text-justify text-xs inline-block">Social Media</p>
             <ul className="flex ml-6">
               <li>
-                <i class="bx bxl-instagram text-xl rounded-full px-3 py-2 text-slate-900 ml-4 border-gray-100	border-2 "></i>
+                <i class="bx bxl-instagram text-xl rounded-full px-3 py-2 text-slate-900 ml-4 border-gray-100	border-2 duration-300 hover:cursor-pointer hover:bg-slate-900 hover:text-white"></i>
               </li>
               <li>
-                <i class="bx bxl-facebook text-xl rounded-full px-3 py-2 text-slate-900 ml-4 border-gray-100	border-2 "></i>{" "}
+                <i class="bx bxl-facebook text-xl rounded-full px-3 py-2 text-slate-900 ml-4 border-gray-100	border-2 duration-300 hover:cursor-pointer hover:bg-slate-900 hover:text-white"></i>{" "}
               </li>
               <li>
-                <i class="bx bxl-instagram text-xl rounded-full px-3 py-2 text-slate-900 ml-4 border-gray-100	border-2 "></i>{" "}
+                <i class="bx bxl-instagram text-xl rounded-full px-3 py-2 text-slate-900 ml-4 border-gray-100	border-2 duration-300 hover:cursor-pointer hover:bg-slate-900 hover:text-white"></i>{" "}
               </li>
             </ul>
           </div>
@@ -34,43 +34,43 @@ const Footer = () => {
           </span>
           <div className="mt-6">
             <div className="flex">
-              <span className="text-sm  basis-1/3 mt-3  text-slate-600">
+              <span className="text-sm  basis-1/3 mt-3  text-slate-600 hover:cursor-pointer hover:text-[#356dff] hover:font-semibold duration-500">
                 Home
               </span>
-              <span className="text-sm basis-1/3 mt-3 text-slate-600">
+              <span className="text-sm  basis-1/3 mt-3  text-slate-600 hover:cursor-pointer hover:text-[#356dff] hover:font-semibold duration-500">
                 Vision
               </span>
-              <span className="text-sm  basis-1/3 mt-3 text-slate-600">
+              <span className="text-sm  basis-1/3 mt-3  text-slate-600 hover:cursor-pointer hover:text-[#356dff] hover:font-semibold duration-500">
                 Privacy & Terms
               </span>
             </div>
             <div className="flex">
-              <span className="text-sm basis-1/3 mt-3 text-slate-600">
+              <span className="text-sm  basis-1/3 mt-3  text-slate-600 hover:cursor-pointer hover:text-[#356dff] hover:font-semibold duration-500">
                 Journey
               </span>
-              <span className="text-sm basis-1/3 mt-3 text-slate-600">
+              <span className="text-sm  basis-1/3 mt-3  text-slate-600 hover:cursor-pointer hover:text-[#356dff] hover:font-semibold duration-500">
                 Thinking
               </span>
-              <span className="text-sm basis-1/3 mt-3 text-slate-600">
+              <span className="text-sm  basis-1/3 mt-3  text-slate-600 hover:cursor-pointer hover:text-[#356dff] hover:font-semibold duration-500">
                 Google
               </span>
             </div>
             <div className="flex">
-              <span className="text-sm basis-1/3 mt-3 text-slate-600">
+              <span className="text-sm  basis-1/3 mt-3  text-slate-600 hover:cursor-pointer hover:text-[#356dff] hover:font-semibold duration-500">
                 Technology
               </span>
-              <span className="text-sm basis-1/3 mt-3 text-slate-600">
+              <span className="text-sm  basis-1/3 mt-3  text-slate-600 hover:cursor-pointer hover:text-[#356dff] hover:font-semibold duration-500">
                 Join us
               </span>
-              <span className="text-sm  basis-1/3 mt-3 text-slate-600">
+              <span className="text-sm  basis-1/3 mt-3  text-slate-600 hover:cursor-pointer hover:text-[#356dff] hover:font-semibold duration-500">
                 Alphabet
               </span>
             </div>
             <div className="flex">
-              <span className="text-sm basis-1/3 mt-3 text-slate-600">
+              <span className="text-sm  basis-1/3 mt-3  text-slate-600 hover:cursor-pointer hover:text-[#356dff] hover:font-semibold duration-500">
                 Team
               </span>
-              <span className="text-sm basis-1/3 mt-3 text-slate-600">
+              <span className="text-sm  basis-1/3 mt-3  text-slate-600 hover:cursor-pointer hover:text-[#356dff] hover:font-semibold duration-500">
                 Connect
               </span>
             </div>
