7e05cd51d7a7c028eadca6891678c858a1714fd6
mina <sadeghbayan.mina@gmail.com>
2023-09-10 15:38:05 +0330

    activeItem in navLinks implemented

--- src/components/Navigation/NavLinks.jsx
+++ src/components/Navigation/NavLinks.jsx
@@ -1,22 +1,80 @@
-import React from 'react'
+import React, {useState} from 'react'
+import style from "./NavLinks.module.css";
 
 const NavLinks = () => {
+  const[activeItem , setActiveItem] = useState(0)
+
   return (
-    <ul className="flex flex-row justify-between hidden lg:flex">
+    <ul className="flex-row justify-between hidden lg:flex">
       <li>
-        <a className="text-sm mx-6 font-bold">Home</a>
+        <a
+          onClick={() => {
+            setActiveItem(0);
+          }}
+          className={`text-sm mx-6 hover:text-slate-900 hover:cursor-pointer  ${
+            activeItem === 0
+              ? `font-bold text-slate-900 ${style.active}`
+              : "font-thin text-zinc-400 "
+          }`}
+        >
+          Home
+        </a>
       </li>
       <li>
-        <a className="text-sm mx-6 text-zinc-400 font-thin">Solutions</a>
+        <a
+          onClick={() => {
+            setActiveItem(1);
+          }}
+          className={`text-sm mx-6 hover:text-slate-900 hover:cursor-pointer  ${
+            activeItem === 1
+              ? `font-bold text-slate-900 ${style.active}`
+              : "font-thin text-zinc-400 "
+          }`}
+        >
+          Solutions
+        </a>
       </li>
       <li>
-        <a className="text-sm mx-6 text-zinc-400 font-thin">Services</a>
+        <a
+          onClick={() => {
+            setActiveItem(2);
+          }}
+          className={`text-sm mx-6  hover:text-slate-900 hover:cursor-pointer ${
+            activeItem === 2
+              ? `font-bold text-slate-900 ${style.active}`
+              : "font-thin text-zinc-400 "
+          }`}
+        >
+          Services
+        </a>
       </li>
       <li>
-        <a className="text-sm mx-6 text-zinc-400 font-thin">About</a>
+        <a
+          onClick={() => {
+            setActiveItem(3);
+          }}
+          className={`text-sm mx-6  hover:text-slate-900 hover:cursor-pointer ${
+            activeItem === 3
+              ? `font-bold text-slate-900 ${style.active}`
+              : "font-thin text-zinc-400 "
+          }`}
+        >
+          About
+        </a>
       </li>
       <li>
-        <a className="text-sm mx-6 text-zinc-400 font-thin">Feedback</a>
+        <a
+          onClick={() => {
+            setActiveItem(4);
+          }}
+          className={`text-sm mx-6  hover:text-slate-900 hover:cursor-pointer ${
+            activeItem === 4
+              ? `font-bold text-slate-900 ${style.active}`
+              : "font-thin text-zinc-400 "
+          }`}
+        >
+          Feedback
+        </a>
       </li>
     </ul>
   );
