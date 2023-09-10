7956b562e6c14bd6d7219c472da08234eaeb3774
mina <sadeghbayan.mina@gmail.com>
2023-09-10 18:15:42 +0330

    card boxShadow changed

--- src/components/UIElements/Card.jsx
+++ src/components/UIElements/Card.jsx
@@ -4,7 +4,9 @@ import React from 'react'
 
 const Card = (props) => {
   return (
-    <div className={`bg-white rounded-r-3xl w-96	 rounded-tl-3xl rounded-bl-md  shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] ${props.style}`}>
+    <div
+      className={`bg-white rounded-r-3xl w-96 rounded-tl-3xl rounded-bl-md  shadow-[rgba(0,_0,_0,_0.2)_0px_20px_40px] ${props.style}`}
+    >
       {props.children}
     </div>
   );
